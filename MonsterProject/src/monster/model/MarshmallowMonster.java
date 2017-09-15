24d32b65dce4465ebecce3a9e316979dde8401e7
maxrichards33 <31513792+maxrichards33@users.noreply.github.com>
2017-09-15 09:34:54 -0600

    added setters and getters

---
+++ MonsterProject/src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,85 @@
+package monster.model;
+
+public class MarshmallowMonster 
+{
+	//Declaration section for the data members
+	private String name;
+	private int eyeCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	private int armCount;
+	
+	public MarshmallowMonster()
+	{
+		//Unless we specify values all data members 
+		//are a zero, false, or null
+	}
+	
+	public MarshmallowMonster(String name, int eyecount, int arms, double tentacleamount, boolean hasbloop)
+	{
+		this.name = name;
+		this.eyeCount = eyecount;
+		this.tentacleAmount = tentacleamount;
+		this.hasBloop = hasbloop;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTenticleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "Hi, I am a scary monster! My name is " + name + " and I have " + eyeCount; 
+		description += " eyes and I have " + armCount + " arms, and I have " + tentacleAmount + " tentacles";
+		description += "and my bloop existe is" + hasBloop;
+						
+		
+		return description;
+	}
+}
